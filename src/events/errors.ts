255535ffd4734fca4c7900f691a58baf2f0c93aa
barrettcosgrove <bcosgrove@umass.edu>
2026-04-13 12:43:53 -0400

    fix: correct error names in EventError union

--- src/events/errors.ts
+++ src/events/errors.ts
@@ -1,27 +1,27 @@
 export type EventError = 
-    | { name: "EventNotFound"; message: string }
-    | { name: "NotAuthorized"; message: string }
-    | { name: "InvalidEventState"; message: string }
-    | { name: "InvalidInput"; message: string }
+    | { name: "EventNotFoundError"; message: string }
+    | { name: "NotAuthorizedError"; message: string }
+    | { name: "InvalidEventStateError"; message: string }
+    | { name: "InvalidInputError"; message: string }
     | { name: "UnexpectedDependencyError"; message: string };
 
 export const EventNotFound = (message: string): EventError => ({
-    name: "EventNotFound",
+    name: "EventNotFoundError",
     message,
 });
 
 export const NotAuthorizedError = (message: string): EventError => ({
-    name: "NotAuthorized",
+    name: "NotAuthorizedError",
     message,
 });
 
 export const InvalidEventStateError = (message: string): EventError => ({
-    name: "InvalidEventState",
+    name: "InvalidEventStateError",
     message,
 });
 
 export const InvalidInputError = (message: string): EventError => ({
-    name: "InvalidInput",
+    name: "InvalidInputError",
     message,
 });
 
