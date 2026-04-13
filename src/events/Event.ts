4e3d748405c32d8e559ad2dbeff7bf544539010e
barrettcosgrove <bcosgrove@umass.edu>
2026-04-12 22:49:02 -0400

    refactor: rename category and timeframe constants

--- src/events/Event.ts
+++ src/events/Event.ts
@@ -1,8 +1,6 @@
 export type EventStatus = "draft" | "published" | "cancelled" | "past";
 
-//Note: This needs to be an array of string since the event cateogry needs to be checked at runtime (types do only exist at compile time)
-//Can come back and add more later just a placeholder for now
-const validEventCategories = [
+const VALID_CATEGORIES = [
     "technology",
     "business",
     "music",
@@ -21,13 +19,11 @@ const validEventCategories = [
     "movies and tv",
     "other"] as const;
 
-export type EventCategory = (typeof validEventCategories)[number];
+export type EventCategory = (typeof VALID_CATEGORIES)[number];
 
-//Note: Same sort of situation here with
-//Again just a placeholder for now
-const validEventTimeframes = ["all", "this_week", "this_month", "this_year"] as const;
+const VALID_TIMEFRAMES = ["all", "this_week", "this_month", "this_year"] as const;
 
-export type EventTimeframe = (typeof validEventTimeframes)[number];
+export type EventTimeframe = (typeof VALID_TIMEFRAMES)[number];
 
 export interface Event {
     id: string
