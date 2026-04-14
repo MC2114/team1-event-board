979707ec76de574844d341aa4f5d63b252a8a72e
Anusha <aarokiaraj@umass.edu>
2026-04-14 09:46:44 -0400

    fix: remove duplicate folder event search repo

--- src/event/EventRepository.ts
+++
@@ -1,8 +0,0 @@
-import type { ICreateEventRecordInput, IEventRecord } from "./Event";
-
-export interface IEventRepository {
-  create(input: ICreateEventRecordInput): Promise<IEventRecord>;
-  findById(id: string): Promise<IEventRecord | null>;
-  listAll(): Promise<IEventRecord[]>;
-  listPublishedUpcoming(now?: Date): Promise<IEventRecord[]>;
-}
