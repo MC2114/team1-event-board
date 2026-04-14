76302501d5fa1ce516bb9019dca52cf62db764ce
Chau Hoang Minh Tran <chtran@umass.edu>
2026-04-14 02:34:14 -0400

    feat: add the event repository interface

--- src/events/EventRepository.ts
+++ src/events/EventRepository.ts
@@ -1,6 +1,10 @@
 import { Ok, Err, type Result } from "../lib/result";
+import { Event } from "./EventTypes";
+import { EventError } from "./errors";
 
 export interface IEventRepository {
     findById(eventId: string): Promise<Result<Event | undefined, EventError>>;
-    
+    findByOrganizer(organizerId: string): Promise<Result<Event[], EventError>>;
+    findAll(): Promise<Result<Event[], EventError>>;
+    save(event: Event): Promise<Result<Event, EventError>>;
 }
