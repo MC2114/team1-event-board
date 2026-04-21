a53744b44dfb3489565ff45c8b54d3b019988e4d
Chau Hoang Minh Tran <chtran@umass.edu>
2026-04-21 16:21:25 -0400

    feat: add helper function makeLogger() to support later RSVP Dashboard tests

---
+++ test/rsvp/RSVPDashboard.test.ts
@@ -0,0 +1,13 @@
+import { Ok } from "../../src/lib/result";
+import { CreateRsvpService } from "../../src/rsvp/RsvpService";
+import type { RSVPWithEvent } from "../../src/rsvp/RSVP";
+import type { ILoggingService } from "../../src/service/LoggingService";
+import {
+    makeEvent,
+    makeEventRepo,
+    makeRsvpRepo,
+} from "../helper/auth";
+
+describe("Feature 7: getRSVPsByUser (Dashboard)", () => {
+    
+})
