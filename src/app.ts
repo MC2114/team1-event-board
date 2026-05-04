6a36d912053f92f3c6cd8a83540dc633207b475b
Anusha <aarokiaraj@umass.edu>
2026-05-03 20:24:52 -0400

    feat: conditionally hide Event List nav button on login and events pages

--- src/app.ts
+++ src/app.ts
@@ -64,6 +64,11 @@ class ExpressApp implements IApp {
     );
     this.app.use(Layouts);
     this.app.use(express.urlencoded({ extended: true }));
+
+    this.app.use((req, res, next) => {
+      res.locals.currentPath = req.path;
+      next();
+    });
   }
 
   private registerTemplating(): void {
